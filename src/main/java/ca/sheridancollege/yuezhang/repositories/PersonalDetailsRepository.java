279a7e8ecd130d95a594fbb674128e1f40fdee64
Cynthia1211 <yue.cynthia.zhang@gmail.com>
2026-06-23 13:21:08 -0400

    remove unused import

--- src/main/java/ca/sheridancollege/yuezhang/repositories/PersonalDetailsRepository.java
+++ src/main/java/ca/sheridancollege/yuezhang/repositories/PersonalDetailsRepository.java
@@ -1,8 +1,4 @@
 package ca.sheridancollege.yuezhang.repositories;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
